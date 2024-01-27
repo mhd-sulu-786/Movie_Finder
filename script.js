04d58f9e7789172a4f9cbc59e8de3da118235d3a
Muhammad Sulaiman T <144138399+mhd-sulu-786@users.noreply.github.com>
2024-01-27 07:25:46 +0000

    script.js

--- script.js
+++ script.js
@@ -19,38 +19,43 @@ let btnSearch = document.getElementById("btnSearch");
 btnSearch.addEventListener("click", Push_data);
 
 function Push_data() {
+    // Reset the link variable to the original API URL
+    link = `https://www.omdbapi.com/?apikey=${accessKey}`;
+
+    // Append the search parameters
     link = link + `&t=${searchValue.value}&plot=full&r=json`;
+
     console.log(link);
+
     fetch(link)
         .then(response => response.json())
         .then(data => {
-            searchValue.value='';
-            title.innerHTML = data.Title;
-            if (data.Title === undefined  || data.Title === "N/A") {
+            searchValue.value = '';
+
+            if (data.Title === undefined || data.Title === "N/A") {
                 alert('Wrong spelling or name');
                 document.getElementById('card_1').style.display = 'none';
             } else {
                 document.getElementById('card_1').style.display = 'block';
                 title.innerHTML = data.Title;
-            
-            // document.getElementById('dscribtion').innerHTML=data.plot;
-            img.innerHTML = `<img src="${data.Poster}" alt="${data.Title}">`;
-            Year.innerHTML = 'Year:-'+data.Year;
-            Actors.innerHTML = 'Actors:-'+ data.Actors;
-            Released.innerHTML = 'Released:-'+ data.Released;
-            Awards.innerHTML = 'Awards:-'+ data.Awards;
-            Country.innerHTML = 'country:-'+ data.Country;
-            Director.innerHTML = 'Director:-'+ data.Director;
-            Language.innerHTML = 'Language:-'+data.Language;
-            Writer.innerHTML = 'Writer:-'+data.Writer;
-            Genre.innerHTML = 'Genre:-'+data.Genre;
-            imdbRating.innerHTML = 'IMDB Raiting:-'+data.imdbRating;
+
+                img.innerHTML = `<img src="${data.Poster}" alt="${data.Title}">`;
+                Year.innerHTML = 'Year: ' + data.Year;
+                Actors.innerHTML = 'Actors: ' + data.Actors;
+                Released.innerHTML = 'Released: ' + data.Released;
+                Awards.innerHTML = 'Awards: ' + data.Awards;
+                Country.innerHTML = 'Country: ' + data.Country;
+                Director.innerHTML = 'Director: ' + data.Director;
+                Language.innerHTML = 'Language: ' + data.Language;
+                Writer.innerHTML = 'Writer: ' + data.Writer;
+                Genre.innerHTML = 'Genre: ' + data.Genre;
+                imdbRating.innerHTML = 'IMDB Rating: ' + data.imdbRating;
             }
         })
         .catch(error => {
-            alert(' Wrong specling or name');
-            searchValue.value='';
+            alert('Error fetching data. Please try again.');
+            searchValue.value = '';
             console.error(error);
-            document.getElementById('card_1').style.display='none';
+            document.getElementById('card_1').style.display = 'none';
         });
 }
